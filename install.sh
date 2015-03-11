fe366c9956ccf53a0959f39be06d2ab2fc41c659
Leonard Truong <leonardtruong@gmail.com>
2015-03-11 10:01:51 -0700

    Ignore symlink

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git submodule update --init
 
 ln -s $PWD/zshrc $HOME/.zshrc
-ln -s "$PWD/zsh/pure/pure.zsh" $PWD/zsh/prompt_pure_setup
+ln -s $PWD/zsh/pure/pure.zsh $PWD/zsh/prompt_pure_setup
 ln -s $PWD/vimrc $HOME/.vimrc
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
